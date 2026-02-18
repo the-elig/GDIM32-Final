f6dc21a63f91657a76eb08ae28779f0892390a8f
R3stl3ssDr3ams <solorio.42707@gmail.com>
2026-02-17 19:13:56 -0800

    GameController

---
+++ Melt ICE/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum _objective
+{
+    empty, cup, key, flamethrower
+}
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+    public Player Player { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        GameObject _player = GameObject.FindWithTag("Player");
+        Player = _player.GetComponent<Player>();
+    }
+}
