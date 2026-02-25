42dd84263d5371467024c10d08ddf64722290c58
the-elig <elidg@uci.edu>
2026-02-24 18:16:18 -0800

    worked inventory and event systems

---
+++ Melt ICE/Assets/Scripts/UIController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _interactText;
+    [SerializeField] private TMP_Text _inventoryText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Locator.Instance.Player.LookingAtInteractable += ShowInteract;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ShowInteract(bool b)
+    {
+        _interactText.enabled = b;
+    }
+}
