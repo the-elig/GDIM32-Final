c9fc99de18972d3b39d13487927c7f93f775a239
R3stl3ssDr3ams <solorio.42707@gmail.com>
2026-02-24 20:18:29 -0800

    Changes to allow the UI to work

--- Melt ICE/Assets/Scripts/UIController.cs
+++ Melt ICE/Assets/Scripts/UIController.cs
@@ -9,6 +9,7 @@ public class UIController : MonoBehaviour
     [SerializeField] private TMP_Text _inventoryText;
     [SerializeField] private GameObject _inventorybox;
     [SerializeField] private TMP_Text _notifText;
+    private bool _boxActive = false;
 
 
     // Start is called before the first frame update
@@ -21,9 +22,15 @@ public class UIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.I) && _boxActive == false)
         {
             _inventorybox.SetActive(true);
+            _boxActive = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && _boxActive == true)
+        {
+            _inventorybox.SetActive(false);
+            _boxActive = false;
         }
     }
 
