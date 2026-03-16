a57e4ffb3510a65605473f7cf3a26655959e1608
R3stl3ssDr3ams <solorio.42707@gmail.com>
2026-03-16 14:42:58 -0700

    Key disappears when talking to neighbor
    It is as the title says.

--- Melt ICE/Assets/Scripts/DontDestroy.cs
+++ Melt ICE/Assets/Scripts/DontDestroy.cs
@@ -23,19 +23,6 @@ public class DontDestroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.activeSelf == false)
-        {
-            for (int i = 0; i < Object.FindObjectsOfType<DontDestroy>().Length; i++)
-            {
-                if (Object.FindObjectsOfType<DontDestroy>()[i] != this)
-                {
-                    if (Object.FindObjectsOfType<DontDestroy>()[i].name == gameObject.name)
-                    {
-                        Destroy(gameObject);
-                        Debug.Log("So this is happening");
-                    }
-                }
-            }
-        }
+        
     }
 }
